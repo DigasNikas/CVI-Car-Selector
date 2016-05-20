dde4cccff032be861017cdd66e9f954d849c1db5
Diogo Nicolau <digasnikas@gmail.com>
2016-05-20 09:48:42 +0100

    image tracking done we need to smoth the background

--- proj.m
+++ proj.m
@@ -5,6 +5,7 @@ str = ['%s%.' num2str(frameIdComp) 'd.%s'];
 
 nFrame = 3064;
 step = 5;
+th = 30;
 
 img = imread('CAMERA1_JPEGS_TRAINING\0001.jpg');
 bkg = zeros(size(img));
@@ -13,15 +14,34 @@ alfa = 0.01;
 for k=1 : step : nFrame
     strl = sprintf(str, path,k,'jpg');
     img = imread(strl);
-   % imshow(img); drawnow
     y = img;
     bkg = alfa * double(y) + (1-alfa) * double(bkg);
-    %imshow(uint8(bkg)); drawnow
 end
-imshow(uint8(bkg));
 
 for k=1 : step : nFrame
     strl = sprintf(str, path,k,'jpg');
     img = imread(strl);
-    %imshow(uint8(bkg)); drawnow
+    se = offsetstrel('ball',5,5);
+    
+    imDiff = (abs(double(img(:,:,1)) - double(bkg(:,:,1))) > th) | (abs(double(img(:,:,2)) - double(bkg(:,:,2))) > th) | (abs(double(img(:,:,3)) - double(bkg(:,:,3))) > th);
+    
+    imDiff = medfilt2(imDiff);
+    imDiff = bwconvhull(imDiff, 'objects');
+    %imDiff = imopen(imDiff, strel('disk',2));
+    %imDiff = imfill(imDiff, 'holes');
+    
+    props = regionprops(imDiff,'BoundingBox', 'Area','MajorAxisLength');
+    
+    strl = sprintf(str, path,k,'jpg');
+    img = imread(strl);
+    imshow(img);
+    
+    text(10,30,int2str(k),'color','r');
+    for k = 1 : length(props)
+      thisBB = props(k).BoundingBox;
+      if(props(k).Area > 100)
+        rectangle('Position', [thisBB(1),thisBB(2),thisBB(3),thisBB(4)],'EdgeColor','r','LineWidth',2 )
+      end
+    end
+    drawnow
 end
